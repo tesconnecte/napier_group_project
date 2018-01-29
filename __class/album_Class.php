deebd38a1e41ca74ab8ac84cc5e09fbdf84788a2
LiamLyons <liam.lyons@laposte.net>
2018-01-29 16:55:40 +0000

    Album class creation

---
+++ __class/album_Class.php
@@ -0,0 +1,18 @@
+<?php
+   class Album {
+       public $id;   // album's id
+       public $name; // name of the Album
+       public $isPublic; // boolean to check if the album is plublic or private
+       public $users; // list of users that have access to the album
+       
+       public function __construct($id, $name, $isPublic, $users)
+       {
+           $this->id = $id;
+           $this->name = $name;
+           $this->isPublic = $isPublic;
+           $this->users = $users;
+       }
+
+
+   }
+?>
--- __class/user_Class.php
+++ __class/user_Class.php
@@ -6,5 +6,16 @@
        public $email; // person's email
        public $password; // person's password
        public $birthDate; // person's birth date
+       public $albums; // list of person's albums
+
+       public function __construct($id, $firstName, $surname, $email, $password, $birthDate, $albums) {
+         $this->id = $id;
+         $this->firstName = $firstName;
+         $this->surname = $surname;
+         $this->email = $email;
+         $this->password = $password; // to change
+         $this->birthDate = $birthDate;
+         $this->albums = $albums; // to change
+       }
    }
 ?>
