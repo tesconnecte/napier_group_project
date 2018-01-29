2913029e69c4f543882760880bef1f7a4c8ce42a
LiamLyons <liam.lyons@laposte.net>
2018-01-29 16:17:41 +0000

    User class update

--- user_Class.php
+++ user_Class.php
@@ -1,6 +1,10 @@
 <?php
    class User {
-       public $id;   // id de la personne
-       public $prenom;  // string. prenom de la personne
+       public $id;   // person's id
+       public $firstName;  // person's $firstName
+       public $surname; // person's $surname
+       public $email; // person's email
+       public $password; // person's password
+       public $birthDate; // person's birth date
    }
 ?>
