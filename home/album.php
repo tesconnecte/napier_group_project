4f0f684291cca253d8cdcd282f51b30c59ec23ff
OtherJack <jacksweeney@outlook.com>
2018-02-19 17:49:44 +0000

    browser issues fixed

--- home/album.php
+++ home/album.php
@@ -1,4 +1,6 @@
-<html>
+<!DOCTYPE html>
+<meta charset="utf-8"/>
+
 <head>
 
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -6,6 +8,7 @@
   <link rel="shortcut icon" type="image/png" href="img/logo2.png"/>
   <link rel="shortcut icon" type="image/png" href="http://eg.com/logo2.png"/>
 
+    <link rel="stylesheet" href="css/reset.css" alt="reset" width="50 px" height="50px">
     <link rel="stylesheet" href="css/style.css" alt="style" width="50 px" height="50px">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script>
@@ -15,21 +18,18 @@
 </head>
 <body>
 
-<!-- navbar -->
-<nav class="navbar">
+  <!-- navbar -->
+  <nav class="navbar">
 
     <div class="container">
 
-        <ul class="nav nav-left">
-            <li><a href="index.php"><h2>Posted</h2></a></li>
-        </ul>
+      <h1 class="logo"><a href="index.php">Posted</a></h1>
+      <ul class="nav nav-right">
+        <li><a href="logIn.php">Log In</a></li>
+      </ul>
+    </div>
 
-        <ul class="nav nav-right">
-            <li><img src="img/logIn.png" height="8%" alt="log in"/><a href="logIn.php">Log In</a></li>
-        </ul>
-
-
-</nav>
+  </nav><!--/.navbar-->
 
 
 </div><!--container-->
