b363a44feee8feeae1b3fc346a7b859087eea1e3
OtherJack <jacksweeney@outlook.com>
2018-02-20 15:30:18 +0000

    User profile page added

---
+++ header/album.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <title>Posted</title>
+  <link rel="shortcut icon" type="image/png" href="../home/img/logo2.png"/>
+  <link rel="shortcut icon" type="image/png" href="http://eg.com/logo2.png"/>
+
+    <link rel="stylesheet" href="../home/css/style.css" alt="style" width="50 px" height="50px">
+    <link rel="stylesheet" type="text/css" href="../home/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,300,700&subset=latin,latin-ext">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Indie+Flower">
+
+</head>
+<body>
+
+<!-- navbar -->
+<nav class="navbar">
+
+    <div class="container">
+
+        <ul class="nav nav-left">
+            <li><a href="../home/index.php"><h2>Posted</h2></a></li>
+        </ul>
+
+        <ul class="nav nav-right">
+            <li><img src="../home/img/logIn.png" height="8%" alt="log in"/><a href="../home/logIn.php">Log In</a></li>
+        </ul>
+
+
+</nav>
+
+
+</div><!--container-->
+</body>
+</html>
--- home/index.php
+++ home/index.php
@@ -1,3 +1,4 @@
+<<<<<<< current
 <?php include ("../header/htmlhead.php"); ?>
 <link rel="stylesheet" href="../home/css/style.css" alt="style" width="50 px" height="50px">
 
@@ -26,3 +27,57 @@ include ("../header/header.php");
 </div><!--container-->
 </body>
 </html>
+=======
+<html>
+<head>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <title>Posted</title>
+
+    <link rel="stylesheet" href="css/style.css" alt="style" width="50 px" height="50px">
+  
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,300,700&subset=latin,latin-ext">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Indie+Flower">
+
+</head>
+<body>
+
+<!-- navbar -->
+<nav class="navbar">
+
+    <div class="container">
+
+        <ul class="nav nav-left">
+            <li><a href="index.php"><h2>Posted</h2></a></li>
+        </ul>
+
+        <ul class="nav nav-right">
+            <li><img src="img/logIn.png" height="8%" alt="log in"/><a href="logIn.php">Log In</a></li>
+        </ul>
+
+
+</nav>
+
+<div class="container">
+    <h1>Welcome to Posted</h1>
+
+    <button type="button" class="btn btn-primary">Sign Up</button>
+
+<!-- Gallery of thumbnails -->
+<div class="featuredAlbums">
+    </br><h3>Featured Albums:</h3>
+        <ul>
+            <li><a href="#"><p>Festival</p><img src="img/fest.jpg" /></li>
+            <li><a href="#"><p>Holiday</p><img src="img/holiday.jpg" /></li>
+            <li><a href="#"><p>Party</p><img src="img/out.jpg" /></li>
+            <li><a href="#"><p>Festival</p><img src="img/fest.jpg" /></li>
+        </ul>
+</div>
+
+</div><!--container-->
+</body>
+</html>
+>>>>>>> before discard
