dd5f8922ce959e336f8c1e5cd1ee2dd9c4e5a9ac
LiamLyons <liam.lyons@laposte.net>
2018-03-06 10:17:51 +0000

    twitter API

---
+++ __class/twitter_Class.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('C:\xampp\htdocs\napier_group_project\__class\twitter-api-php-master\test\TwitterAPIExchangeTest.php');
+
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "YOUR_OAUTH_ACCESS_TOKEN",
+    'oauth_access_token_secret' => "YOUR_OAUTH_ACCESS_TOKEN_SECRET",
+    'consumer_key' => "YOUR_CONSUMER_KEY",
+    'consumer_secret' => "YOUR_CONSUMER_SECRET"
+);
