700168a6e853dc65bb427c5ee702433292a0c521
LiamLyons <liam.lyons@laposte.net>
2018-03-08 10:53:49 +0000

    Twitter API update

--- __class/twitter_Class.php
+++ __class/twitter_Class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('C:\xampp\htdocs\napier_group_project\__class\twitter-api-php-master\test\TwitterAPIExchangeTest.php');
+require_once('C:\xampp\htdocs\napier_group_project\__class\twitter-api-php-master\TwitterAPIExchange.php');
 
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 $settings = array(
@@ -9,3 +9,10 @@ $settings = array(
     'consumer_key' => "YOUR_CONSUMER_KEY",
     'consumer_secret' => "YOUR_CONSUMER_SECRET"
 );
+
+$url = "https://publish.twitter.com/oembed?url=https://twitter.com/Twitter/status/970476456139608064";
+$requestMethod = "GET";
+
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->buildOauth($url, $requestMethod)
+    ->performRequest();
