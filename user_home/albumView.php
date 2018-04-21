638fd83ebc27b64e4ca06ddb8269fc14ef4cf6b1
LiamLyons <liam.lyons@laposte.net>
2018-04-21 15:05:40 +0100

    Album view done

--- user_home/albumView.php
+++ user_home/albumView.php
@@ -54,113 +54,57 @@ if(!isset($_GET['id'])){
 
             <h3>Click to edit posts</h3>
             <?php
-            for ($i = 0; $i < count($posts); ++$i) {
 
                 //COMPLETE HERE
                 if (count($posts) == 0) {
                     echo("<div><p>No posts in this album</p></div>");
                 }
-                for ($j = 0; $j < count($posts); ++$j) {
-                    $current_post = $posts[$j];
-                    if (!empty($current_post->getLink())) {
-                        echo(" <div class=\"gallery-item\">");
-                        $link = $current_post->getLink();
-                        if (strpos($link, 'facebook') !== false) {
-                            echo("<div class=\"fb-post\"data-href=\"" . $link . "\"></div>");
-                        } elseif (strpos($link, 'twitter') !== false) {
-                            //echo($link);
-                            $curl = curl_init();
-                            curl_setopt_array($curl, array(
-                                CURLOPT_RETURNTRANSFER => 1,
-                                CURLOPT_URL => 'https://publish.twitter.com/oembed?url=' . $link
-                            ));
-                            $result = curl_exec($curl);
-                            curl_close($curl);
-                            $result = json_decode($result, true);
-                            echo($result['html']); // Displays the embedded tweet
+                else {
+                    for ($j = 0; $j < count($posts); ++$j) {
+                        $current_post = $posts[$j];
+                        if (!empty($current_post->getLink())) {
+                            echo("<a href=\"../user_home/editPost.php?id=".$current_post->getId()."\">");
+                            echo(" <div class=\"gallery-item\">");
+                            $link = $current_post->getLink();
+                            if (strpos($link, 'facebook') !== false) {
+                                echo("<div class=\"fb-post\"data-href=\"" . $link . "\"></div>");
+                            } elseif (strpos($link, 'twitter') !== false) {
+                                //echo($link);
+                                $curl = curl_init();
+                                curl_setopt_array($curl, array(
+                                    CURLOPT_RETURNTRANSFER => 1,
+                                    CURLOPT_URL => 'https://publish.twitter.com/oembed?url=' . $link
+                                ));
+                                $result = curl_exec($curl);
+                                curl_close($curl);
+                                $result = json_decode($result, true);
+                                echo($result['html']); // Displays the embedded tweet
 
-                        } elseif (strpos($link, 'instagram') !== false) {
-                            echo("<blockquote class=\"instagram-media\" data-instgrm-captioned data-instgrm-permalink=\"" . $link . "\" data-instgrm-version=\"8\" ></blockquote>");
+                            } elseif (strpos($link, 'instagram') !== false) {
+                                echo("<blockquote class=\"instagram-media\" data-instgrm-captioned data-instgrm-permalink=\"" . $link . "\" data-instgrm-version=\"8\" ></blockquote>");
+                            } else {
+                                echo(" <p>" . $current_post->getDescription() . "</p>");
+                                echo(" <img src='../__website_content/no_image.png'/>");
+                            }
+                            echo("</div></a>");
                         } else {
+                            echo("<a href=\"../user_home/editPost.php?id=".$current_post->getId()."\">");
+                            echo(" <div class=\"gallery-item\">");
                             echo(" <p>" . $current_post->getDescription() . "</p>");
-                            echo(" <img src='../__website_content/no_image.png'/>");
+                            if ($current_post->getImage() != null) {
+                                echo(" <img src='../'" . $current_post->getImage() . "/>");
+                            } else {
+                                echo(" <img src='../__website_content/no_image.png'/>");
+                            }
+                            echo(" </div></a>");
                         }
-                        echo("</div>");
-                    } else {
-                        echo(" <div class=\"gallery-item\">");
-                        echo ("<a href=\"../user_home/editPost.php\">");
-                        echo(" <p>" . $current_post->getDescription() . "</p>");
-                        if ($current_post->getImage()!=null) {
-                            echo(" <img src='../__website_content/no_image.png'/>");
-                        }
-                        echo(" <img src='../__website_content/no_image.png'/>");
-                        echo(" </a></div>");
                     }
                 }
-            }
+
 
 
             ?>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image one" title="Gallery Image One"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image two"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image three"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image four"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image five"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image six"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image seven"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image eight"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image nine"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image ten"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image eleven"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image twelve"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image thirteen"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image fourteen"/>
-                </a></div>
-            <div class="gallery-item">
-                <a href="../user_home/editPost.php">
-                    <img src="../__website_content/lorem.jpg" alt="gallery image fifteen"/>
-                </a></div>
+
         </div><!--gllery -->
 
         <div class="loadMore">
